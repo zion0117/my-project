fb06b7d634ce042f178476dddc1ae67a0a576109
su yeon kim <65233714+zion0117@users.noreply.github.com>
2024-12-17 09:37:07 +0900

    Revert "update workout.js"
    This reverts commit fad9e960740168b3f7f3c4748057a329a87f00bb.

--- src/workout.js
+++ src/workout.js
@@ -207,6 +207,7 @@ function Workout() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
       <h1>운동 자세 분석</h1>
+      <h2>현재 프레임: {currentFrame + 1}</h2>
       <h3>점수: {Math.round(score)}</h3> {/* 총 점수 출력 */}
       <h3>피드백: {feedback}</h3>
       {exerciseComplete ? (
