d3181310b0675586702c480f3eba5a13dc02f6df
su yeon kim <65233714+zion0117@users.noreply.github.com>
2025-02-21 00:00:08 +0900

    clientid

--- project/app/(tabs)/GoogleSignInButton.js
+++ project/app/(tabs)/GoogleSignInButton.js
@@ -3,15 +3,15 @@ import { TouchableOpacity, Text, StyleSheet } from "react-native";
 import * as Google from "expo-auth-session/providers/google";
 import * as WebBrowser from "expo-web-browser";
 import { auth } from "./firebaseConfig";
-import { getAuth } from "firebase/auth"; 
+import { getAuth } from "firebase/auth";
 import { signInWithCredential, GoogleAuthProvider } from "firebase/auth";
 
 WebBrowser.maybeCompleteAuthSession(); // ✅ 웹 브라우저 로그인 완료 처리
 
 const GoogleSignInButton = () => {
   const [request, response, promptAsync] = Google.useAuthRequest({
-    expoClientId: "YOUR_EXPO_CLIENT_ID", // ✅ Firebase 콘솔에서 가져온 Expo Client ID
-    androidClientId: "YOUR_ANDROID_CLIENT_ID", // ✅ Android Client ID
+    expoClientId: "https://auth.expo.io/@suyeon11/app.json", // ✅ Firebase 콘솔에서 가져온 Expo Client ID
+    androidClientId: "193222581186-n71h707adtk4rvjfikqkc0jt1h4mgcva.apps.googleusercontent.com", // ✅ Android Client ID
     webClientId: "YOUR_WEB_CLIENT_ID",   // ✅ Web Client ID
   });
 
